33462c46b65c9a76fb88847161582471b21f8fbd
ShreyasGanesh911 <shreyas.ganesh911@gmail.com>
2024-05-03 14:47:02 +0530

    Added login component

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,23 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import SignupForm from "../components/SignupForm";
+import OtpField from "../components/OtpField";
+import Login from "../components/Login";
 
 
 export default function Home() {
+  const [screen,setScreen] = useState(false)
+  const [login,setLogin] = useState(true)
   return (
     <>
       <section
         className="page bg-slate-50 displayFlex home"
-        style={{
-          backgroundImage:
-            "https://i.pinimg.com/736x/b4/3d/91/b43d91b07d2648e8d85d2d94c0e96135.jpg",
-        }}
+        
       >
         <div
           className="lg:w-3/4 lg:h-3/4  w-11/12  border lg:bg-white rounded-3xl flex items-center justify-evenly"
           style={{
             height: "80vh",
-            backgroundImage:
-              "https://i.pinimg.com/736x/b4/3d/91/b43d91b07d2648e8d85d2d94c0e96135.jpg",
+           
           }}
         >
           <div className="lg:w-2/5 lg:h-5/6  lg:flex lg:justify-center lg:items-center hidden lg:flex-col bg-slate-200">
@@ -25,8 +25,21 @@ export default function Home() {
           <p className="mt-5 text-xl p-3">The one stop <span className="text-violet-700">shop</span></p>
           </div>
           <div className="lg:w-2/5 lg:h-5/6 w-full overflow-y-hidden lg:p-0 bg-black text-white rounded-lg">
-            <h1 className="p-3 ml-3 lg:text-5xl text-4xl">Get started</h1>
-            <SignupForm />
+            {!screen?
+                       <>
+                       {
+                          login ? <>
+                            <h1 className="p-2 ml-3 lg:text-5xl text-4xl">Get started</h1>
+                            <p className="w-full  text-center lg:text-start lg:mx-10">Have an account <button onClick={()=>setLogin(false)}><span className="text-violet-300">Login now</span></button></p>
+                        <SignupForm screen={screen} setScreen={setScreen}/>
+                          </>:<>
+                            <Login screen={login} setScreen={setLogin}/>
+                          </>
+                       }
+                       </>
+                       : <OtpField/>
+
+          }
           </div>
         </div>
       </section>
