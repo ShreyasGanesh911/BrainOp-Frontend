71aaf0d45c55db499af0d3c67476ce70d881a939
ShreyasGanesh911 <shreyas.ganesh911@gmail.com>
2024-05-03 11:54:01 +0530

    Added types

---
+++ src/Types/ProductTypes.ts
@@ -0,0 +1,11 @@
+export type Props = {
+    title:String,
+    description:String,
+    price:number,
+    brand:String,
+    images:string[],
+    rating:Number,
+    stock:Number,
+    category:String,
+    discountPercentage:number
+  }
